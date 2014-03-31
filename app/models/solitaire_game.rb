c240c273c8f2ff31144d626d8021e1df0d021c2e
fernandacoto <fermarioro@gmail.com>
2014-03-31 12:25:48 -0500

    Initializing product line on new game

--- app/models/solitaire_game.rb
+++ app/models/solitaire_game.rb
@@ -2,11 +2,12 @@ class SolitaireGame < ActiveRecord::Base
   before_create :init_everything
 
   has_one :feeder_line
+  has_one :product_line
 
   def init_everything
     @cards = Card.all.shuffle!
     init_feeder_line
-    #init_product_line
+    init_product_line
     #init_cards_train
   end
 
@@ -15,5 +16,10 @@ class SolitaireGame < ActiveRecord::Base
     self.feeder_line = FeederLine.new
     cards = @cards.pop(28)
     feeder_line.add_lines(cards)
+  end
+
+  def init_product_line
+    self.product_line = ProductLine.new
+    product_line.add_lines
   end
 end
