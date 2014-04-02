b0e1d0d346aeeb382f40dd6551bb0e913dde0cd8
fernandacoto <fermarioro@gmail.com>
2014-04-02 14:25:33 -0500

    test when origin is product line and destiny is feeder line

--- spec/solitaire/game/card_mover_spec.rb
+++ spec/solitaire/game/card_mover_spec.rb
@@ -1,19 +1,18 @@
 require 'solitaire/game/card_mover'
-
+require 'spec_helper'
 describe Solitaire::Game::CardMover, "#destiny column" do
   let(:game) { double(:game) }
-  let(:card) { double(:card) }
+  let(:card) { Card.create}
   let(:cards) { double(:cards_collection_proxy, find_by: card) }
 
-  context "when destiny is to feeder line" do
-    let(:feeder_line_column) { double(:feeder_line_column, accept_move?: true)}
+  context "when origin is product line and destiny is feeder line" do
+    let(:feeder_line_column) { double(:feeder_line_column, accept_move?: true, add_cards: card) }
+    let(:product_line_column) { double(:product_line_column, cards_from: [card], remove_cards: card )}
     let(:params){ {
-      origin: { column: '13', card: '2' },
+      origin: { column: product_line_column, card: card },
       destiny: { column: feeder_line_column, card: '6' }
     } }
-
-    subject{ described_class.new(params)}
-
+    subject{ described_class.new(params) }
     its(:move_cards) { should == true }
   end
 
