63ee3fbc4656b24276e4a200971be219205a83a6
satchiss08 <satchiss08@gmail.com>
2014-04-02 17:05:16 -0500

    Finish testing for card_mover

--- spec/solitaire/game/card_mover_spec.rb
+++ spec/solitaire/game/card_mover_spec.rb
@@ -1,7 +1,7 @@
 require 'solitaire/game/card_mover'
-describe Solitaire::Game::CardMover, "#destiny column" do
+describe Solitaire::Game::CardMover, "#move_cards" do
   let(:destiny_column) { double(:destiny_column).as_null_object }
-  let(:origin_column) { double(:destiny_column).as_null_object }
+  let(:origin_column) { double(:origin_column).as_null_object }
 
   let(:origin_card) { double(:origin_card) }
   let(:destiny_card) { double(:destiny_card) }
@@ -19,11 +19,12 @@ describe Solitaire::Game::CardMover, "#destiny column" do
   end
 
   context "when destiny column accepts the move" do
-    let(:cards_to_move) { [double(:card)].as_null_object }
+    let(:cards_to_move) { [double(:card)] }
     let(:card) { double(:card) }
 
     before do
       allow(destiny_column).to receive(:accept_move?) { true }
+      allow(origin_column).to receive(:cards_from) { cards_to_move }
     end
 
     it "gets the cards to move" do
@@ -32,12 +33,12 @@ describe Solitaire::Game::CardMover, "#destiny column" do
     end
 
     it "removes the cards from the origin column" do
-      expect(origin_column).to receive(:remove_cards).with(origin_column) { card }
+      expect(origin_column).to receive(:remove_cards).with(cards_to_move) { card }
       subject.move_cards
     end
 
     it "adds cards to destiny column" do
-      expect(destiny_column).to receive(:add_cards).with(origin_column) { card }
+      expect(destiny_column).to receive(:add_cards).with(cards_to_move) { card }
       subject.move_cards
     end
     it "returns true" do
