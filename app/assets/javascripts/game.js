caee112db428810e155e351bc63b22abbcbe8e69
Erick Castillo <erickcsh@gmail.com>
2014-04-07 14:02:46 -0600

    Dragging cards for feeder line cards and not droppable on train

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -13,7 +13,7 @@ $("#next-card-trigger").click(function(){
   $(this).parent().submit();
 });
 
-$(".empty-column").click(function(){
+/*$(".empty-column").click(function(){
   if($("#origin_card_id").val()  != "") {
     originCard = $(".card-selected");
     originCard.removeClass();
@@ -36,33 +36,110 @@ $("body").delegate('.card', 'click', function(){
   } else {
     console.log($(this));
     selectRelevantCards($(this));
-    $(this).hasClass("card-selected") ? processSelectedCard($(this)) : deleteCard($(this));
+    $(this).hasClass("card-selected") ? processSelectedCard($(this)) : deleteCard($(this));*/
+
+var draggableOptions = {
+    revert: function(isValidMove) {
+      if(!isValidMove) {
+        $('.dragging-card').detach().css({top: 0,left: 0}).appendTo($('.original-parent'));
+      }
+      return !isValidMove;
+    },
+    start: function(event, ui) {
+      $('.last-card').droppable({ disabled: true });
+    },
+    helper: function(event, ui) {
+      $(this).parent().addClass('original-parent');
+      selectDraggingCards($(this));
+      var draggingGroup = $('<div />', { "class": 'dragging-group feeder_line_column'});
+      draggingGroup.appendTo($(this).parent());
+      $('.dragging-card').detach().appendTo(draggingGroup);
+      return draggingGroup;
+    },
+    stop: function(event, ui) {
+      $('div.dragging-card').removeClass('dragging-card')
+      enableDroppableLastCard();
+      $('.last-card').removeClass('last-card');
+      $('.original-parent').removeClass('original-parent');
+    }
+};
+
+$( ".card" ).not(".card-hidden").draggable(draggableOptions);
+
+function enableDroppableLastCard() {
+  var lastCard = $('.last-card')
+  if(lastCard.parent().attr('id') != 'train') {
+    lastCard.droppable({ disabled: false });
+  }
+}
+
+function selectDraggingCards(card) {
+  topMostCard(card);
+  card.addClass('dragging-card');
+  selectLastDraggingCard(card);
+  card.nextAll().each(function(index) {
+    topMostCard($(this));
+    $(this).addClass('dragging-card');
+  });
+};
+
+function selectLastDraggingCard(card) {
+  if(card.is(':last-child')) {
+    card.addClass('last-card');
+  } else {
+    card.nextAll().last().addClass('last-card');
+  }
+};
+
+$( ".card" ).droppable({
+  drop: function(event, ui) {
+    createOriginCardID(ui.draggable);
+    createDestinyCardID($(this));
+    $('.dragging-card').detach().css({top: 0,left: 0}).appendTo($(this).parent());
+    addDroppableInOriginColumn();
+    removeDroppableDestinyCard($(this));
+    moveOriginToDestiny($(this), ui.draggable);
   }
 });
 
-function processSelectedCard(card) {
-  if($("#origin_card_id").val()) {
-    createDestinyCardID(card);
-    moveOriginToDestiny(card);
-  } else {
-    createOriginCardID(card);
+function addDroppableInOriginColumn() {
+  var last = $('.original-parent div.card:nth-last-child(2)');
+  last.droppable({ disabled: false });
+}
+
+function removeDroppableDestinyCard(card) {
+  card.droppable({ disabled: true });
+}
+
+function disableNotDroppableCards() {
+  $('.card').droppable({ disabled: true });
+  $('.feeder_line_column div.card:last-child').droppable({ disabled: false });
+}
+
+$(document).ready(function() {
+  disableNotDroppableCards();
+});
+
+$(".empty-column").droppable({
+  drop: function(event, ui) {
+    createOriginCardID(ui.draggable);
+    $('dragging-card').detach().css({top: 0,left: 0}).appendTo($(this));
+    if($("#origin_card_id").val()) {
+      originCard = $(".card-selected");
+      originCard.removeClass();
+      columnID = $("#destiny_column_id").val();
+      originCard.addClass("card card-selected product-line-card");
+
+      destinyColumnID = $("#destiny_column_id");
+      destinyColumnID.val($(this).attr('id'));
+      $(this).empty();
+      $(this).append(originCard);
+
+      $("#destiny").text(destinyColumnID.val());
+      //$(".edit_solitaire_game").submit();
+    }
   }
-};
-
-function deleteCard(card) {
-  destinyCardID = $("#destiny_card_id");
-  source =  (destinyCardID.val() == card.attr('id')) ? "destiny" : "origin"
-  deleteCardID("source");
-};
-
-function deleteCardID(source) {
-  cardID = $("#"+source+"_card_id");
-  columnID = $("#destiny_column_id");
-
-  cardID.removeAttr('value')
-  columnID.removeAttr('value')
-  $("#"+source).text("");
-};
+});
 
 function createDestinyCardID(cardDiv) {
   destinyCardID = $("#destiny_card_id");
@@ -81,40 +158,35 @@ function createOriginCardID(cardDiv) {
   console.log(cardDiv.attr('id'));
 
   originCardID.val(cardDiv.attr('id'));
-  originColumnID.val(cardDiv.parent().attr('id'));
+  originColumnID.val($('.original-parent').attr('id'));
 
   $("#origin").text(originCardID.val() + " -> ");
 };
 
-function moveOriginToDestiny(destinyCard) {
-  originCard = $(".card-selected");
-  originCard.removeClass();
+function moveOriginToDestiny(destinyCard, originCard) {
   columnID = $("#destiny_column_id").val();
-  originCard.addClass("card card-selected");
 
   if(columnID.match(/product_line_column/)){
-    originCard.addClass("card card-selected product-line-card");
+    originCard.addClass("product-line-card");
   }
 
-  destinyCard.after(originCard);
-  $(".move-card-form").submit();
+  //destinyCard.after(originCard);
+  //$(".move-card-form").submit();
+  $(".edit_solitaire_game").submit();
 };
 
-function style_children(parent, select) {
-  parent.toggleClass("card-selected");
-  parent.children().each(function() {
-    if($(this).hasClass("card")){
-      style_children($(this));
+function topMostCard(card)
+{
+    var elements = document.getElementsByClassName("card");
+    var highest_index = 0;
+
+    for (var i = 0; i < elements.length - 1; i++) {
+        if (parseInt(elements[i].style.zIndex) > highest_index) {
+            highest_index = parseInt(elements[i].style.zIndex);
+        }
     }
-  });
-};
-
-function selectRelevantCards(card){
-  card.toggleClass("card-selected");
-  card.nextAll().each(function() {
-    style_children($(this));
-  });
-};
+    card.css('zIndex', highest_index + 1);
+}
 
 $("#train-card").click(function(){
   $(this).toggleClass("train-card-selected");
