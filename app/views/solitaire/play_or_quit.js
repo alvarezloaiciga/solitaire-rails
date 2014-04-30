14876bee6ec3437f4c473299e3f95a32b718edf5
fernandacoto <fermarioro@gmail.com>
2014-04-30 09:53:36 -0600

    redirecting to create a new game

--- app/views/solitaire/play_or_quit.js
+++ app/views/solitaire/play_or_quit.js
@@ -1,1 +1,6 @@
-alert("Done!");
+var response = confirm("Do you wanna play again or quit? Click ok to play again or cancel to quit")
+if(response == true)
+  {
+    window.location.href = "http://localhost:3000/solitaire/new";
+  }
+
